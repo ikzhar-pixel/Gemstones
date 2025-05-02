764b545933a1287385fb6194281c2fc311ab3d53
Ivan Zharikov <ik.zharikov@gmail.com>
2025-05-02 17:46:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const navbar = document.querySelector(".navbar");
-const toggleBtn = document.querySelector(".toggle-btn");
+const navbar = document.querySelector('.navbar');
+const toggleBtn = document.querySelector('.toggle-btn');
 
-toggleBtn.addEventListener("click", () => {
-    navbar.classList.toggle("active");
+toggleBtn.addEventListener('click', (e) => {
+    e.stopPropagation(); 
+    navbar.classList.toggle('active');
 
-    if (navbar.classList.contains("active")) {
-        toggleBtn.classList.replace("bx-menu", "bx-x");
+    if (toggleBtn.classList.contains('bx-menu')) {
+        toggleBtn.classList.remove('bx-menu');
+        toggleBtn.classList.add('bx-x');
     } else {
-        toggleBtn.classList.replace("bx-x", "bx-menu");
+        toggleBtn.classList.remove('bx-x');
+        toggleBtn.classList.add('bx-menu');
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (navbar.classList.contains('active') && !navbar.contains(e.target)) {
+        navbar.classList.remove('active');
+
+        toggleBtn.classList.remove('bx-x');
+        toggleBtn.classList.add('bx-menu');
     }
 });
